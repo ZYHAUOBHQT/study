39c084faae49d40db940fd9acc712ce92751f6aa
scott_zhu <scott_zhu@example.com>
2018-12-19 17:24:45 +0800

    20181218001

---
+++ src/main/java/com/zbq/CabiBcBi_Java8DefaultMethod_20181218/Java8Tester.java
@@ -0,0 +1,33 @@
+package com.zbq.CabiBcBi_Java8DefaultMethod_20181218;
+
+public class Java8Tester {
+    public static void main(String args[]) {
+        Vehicle vehicle = new Car();
+        vehicle.print();
+    }
+}
+
+interface Vehicle {
+    default void print() {
+        System.out.println("我是一辆车!");
+    }
+
+    static void blowHorn() {
+        System.out.println("按喇叭!!!");
+    }
+}
+
+interface FourWheeler {
+    default void print() {
+        System.out.println("我是一辆四轮车!");
+    }
+}
+
+class Car implements Vehicle, FourWheeler {
+    public void print() {
+        Vehicle.super.print();
+        FourWheeler.super.print();
+        Vehicle.blowHorn();
+        System.out.println("我是一辆汽车!");
+    }
+}
---
+++ src/main/java/com/zbq/CabiBcBi_Java8LambdaExpression_20181218/Java8Tester2.java
@@ -0,0 +1,16 @@
+package com.zbq.CabiBcBi_Java8LambdaExpression_20181218;
+
+public class Java8Tester2 {
+
+    final static String salutation = "Hello! ";
+
+    public static void main(String args[]) {
+        GreetingService greetService1 = message ->
+                System.out.println(salutation + message);
+        greetService1.sayMessage("Runoob");
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+}
