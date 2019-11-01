8aca11a55418b8109bab8388335c984a05c4f0c3
ZYHAUOBHQT <1055793362@qq.com>
2019-11-01 14:47:27 +0800

    20191101

--- src/main/java/com/zbq001_JavaBasics/CabiBcBe_HanoiAlgorithm_20181214/MainClass.java
+++ src/main/java/com/zbq001_JavaBasics/CabiBcBe_HanoiAlgorithm_20181214/MainClass.java
@@ -5,14 +5,15 @@ public class MainClass {
         int nDisks = 5;
         doTowers(nDisks, 'A', 'B', 'C');
     }
+
     public static void doTowers(int topN, char from, char inter, char to) {
-        if (topN == 1){
+        if (topN == 1) {
             System.out.println("Disk 1 from "
-            + from + " to " + to);
-        }else {
+                    + from + " to " + to);
+        } else {
             doTowers(topN - 1, from, to, inter);
             System.out.println("Disk "
-            + topN + " from " + from + " to " + to);
+                    + topN + " from " + from + " to " + to);
             doTowers(topN - 1, inter, from, to);
         }
     }
